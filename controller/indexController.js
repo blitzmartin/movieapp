10242cf75df00254287ba36ca83070e652c8074d
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-12 18:58:01 +0200

    small changes

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,6 +1,6 @@
 const showHomepage = function (req, res) {
     res.render('index');
-    console.log("Utility msg: homepage routing working!")
+    console.log("Utility msg: index routing working!")
 }
 
 module.exports = {showHomepage};
