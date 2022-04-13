4bd98770335f3e49711ec784b88d8aabbd9fe99b
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-13 14:25:07 +0200

    small changes

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String },
-    password: {type: String}
+    password: { type: String }
 })
 
 module.exports = new mongoose.model('User', userSchema);
