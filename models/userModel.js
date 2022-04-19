dbda3b60ea8fe17202495aeddd63c28ea5e49527
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-19 16:53:42 +0200

    small changes

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String },
     password: { type: String },
-    favprite: [ {
+    favorite: [ {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Movie",
         default : []
