7912c6463ebc45a8113f37df8a804127b83e79a7
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-19 16:43:55 +0200

    added favorite movies to user schema

--- models/userModel.js
+++ models/userModel.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String },
-    password: { type: String }
-   /*  favorites: [{type: String}] */
+    password: { type: String },
+    favprite: [ {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Movie",
+        default : []
+    }]
 })
 
 module.exports = new mongoose.model('User', userSchema);
